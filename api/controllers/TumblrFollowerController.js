ea9efb1f5a546b98cf57a0af68f18e0681d86e48
troyth <troyth@gmail.com>
2014-07-14 22:34:31 -0400

    tumblrFollower

--- api/controllers/TumblrFollowerController.js
+++ api/controllers/TumblrFollowerController.js
@@ -15,16 +15,30 @@
  * @docs        :: http://sailsjs.org/#!documentation/controllers
  */
 
-module.exports = {
-    
-  
+var tumblrFollowerController = {
 
+  'index': function(req, res){
 
-  /**
-   * Overrides for the settings in `config/controllers.js`
-   * (specific to TumblrFollowerController)
-   */
-  _config: {}
+    var tumblr = require('tumblr.js');
 
-  
+    var tumblrClient = tumblr.createClient({
+      consumer_key: 'AlGQ1aWiD6D2M5alqTbeM5Et7wQR0e9OvixCtAT9YpDqKCK3bI',
+      consumer_secret: 'AfRbdjWLOzcghJJr7u4YFjYTIMT9hPwq9Eb8n4BqwOim5UtV29',
+      token: 'dMwGcfLnaQ41Cb6GDQOD0CCZdEr82p5lvAbR2ltnDMl7rRS1Gs',
+      token_secret: 'VkQrDv7EBPAwCwPLCC4G6nK0yATmCeuV4TBNBrymdi3OEztXNv'
+    });
+
+    tumblrClient.followers('theenergyissue', function (err, data) {
+      console.log('followers data:');
+      console.dir(data);
+
+      var followers = data.total_users;
+
+      res.view({title: 'Tumblr', followers: followers, name: "Weekly Following Goal" });
+    });
+
+  }
+
 };
+
+module.exports = tumblrFollowerController;
